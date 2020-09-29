70accd6e441fd535d32003c40bca714a0e472fc6
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-09-30 00:16:38 +0900

    Update logo button

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AppBar, Toolbar } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 import Logo from "./Logo";
 import LayoutAccount from "./LayoutAccount";
 
@@ -7,12 +8,13 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 function Layout({ children }: LayoutProps) {
+  const history = useHistory();
   return (
     <>
       <header>
         <AppBar position="static" style={{ backgroundColor: "white" }}>
           <Toolbar>
-            <Logo size={30} />
+            <div style={{display: 'inline-block', cursor: 'pointer'}} onClick={() => history.push('/')}><Logo size={30} /></div>
             <div
               style={{ flex: 1, display: "flex", justifyContent: "flex-end" }}
             >
